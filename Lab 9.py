73cc2757441aa2b65ec453cb231e1486caa1cae0
Adri-2424 <adriannajordyn.grant@yahoo.com>
2024-07-23 21:53:20 -0400

    updated

--- Lab 9.py
+++ Lab 9.py
@@ -5,3 +5,17 @@ def encode(password):
         newPassword.append(str(newChar))
     return "".join(newPassword)
 
+
+def main():
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    option = input("Please enter an option:")
+
+    if option == 1:
+        password = input("Please enter the password to encode: ")
+        print(encode(password))
+    if option == 2:
+        password = input("Please enter the password to decode: ")
