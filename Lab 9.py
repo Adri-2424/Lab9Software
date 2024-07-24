6bb8768ab5d0125652f523cd2fcdb4d714f702dc
Adri-2424 <adriannajordyn.grant@yahoo.com>
2024-07-23 20:28:03 -0400

    "comitted!"

---
+++ Lab 9.py
@@ -0,0 +1,7 @@
+def encode(password):
+    newPassword = []
+    for char in password:
+        newChar = (int(char) + 3)%10
+        newPassword.append(str(newChar))
+    return "".join(newPassword)
+
