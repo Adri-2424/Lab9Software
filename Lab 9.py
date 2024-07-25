05be6cdc35cf6c009a69d7e974a9f036a32d3276
Adri-2424 <adriannajordyn.grant@yahoo.com>
2024-07-25 13:06:54 -0400

    final work

--- Lab 9.py
+++ Lab 9.py
@@ -12,3 +12,27 @@ def decode(password):
     for i in range(len(password)):
         newPassword += str(int(password[i]) - 3)
     return newPassword
+
+def main():
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        option = int(input(("Please enter an option: ")))
+
+        if option == 1:
+            password = input("Please enter the password to encode: ")
+            encoded = encode(password)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            decoded = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoded}.")
+        elif option == 3:
+            break
+        else:
+            print("Invalid option.")
+
+if __name__ == "__main__":
+    main()
