21896bee9631e30205603ad93cc84bc7a175afcf
amandacmwood <127638111+amanda-cw@users.noreply.github.com>
2024-07-25 10:44:43 -0400

    Added Decode Function

--- Lab 9.py
+++ Lab 9.py
@@ -1,12 +1,14 @@
 def encode(password):
     newPassword = []
     for char in password:
-        newChar = (int(char) + 3)%10
+        newChar = (int(char) + 3) % 10
         newPassword.append(str(newChar))
     return "".join(newPassword)
 
-#decode code 
 
-
-
-
+# decode code
+def decode(password):
+    newPassword = ""
+    for i in range(len(password)):
+        newPassword += str(int(password[i]) - 3)
+    return newPassword
